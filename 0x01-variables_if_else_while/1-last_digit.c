966d07dd502ed74c916e741353a98457db248dc8
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-08 06:14:15 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,18 +9,21 @@
  */
 int main(void)
 {
-int n;
+int n, last_digit;
 
 srand(time(0)); /* Seed the random number generator */
 n = rand(); /* Generate a random number and store it in n */
+last_digit = n % 10;
+if (n < 0)
+last_digit = -last_digit;
 
 printf("Last digit of %d is %d",
 n,
-abs(n) % 10); /* Print generated number and its last digit */
+last_digit); /* Print generated number and its last digit */
 
-if (abs(n) % 10 > 5)
+if (last_digit > 5)
 printf(" and is greater than 5\n");
-else if (abs(n) % 10 == 0)
+else if (last_digit == 0)
 printf(" and is 0\n");
 else
 printf(" and is less than 6 and not 0\n");
