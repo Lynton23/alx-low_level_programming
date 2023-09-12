935a4c1d755e04d3364960d4f962add947adae2e
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-12 11:34:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototype for _putchar */
 int _putchar(char c);
 
+/* Function prototype for print_alphabet */
+void print_alphabet(void);
+
 #endif /* MAIN_H */
+
