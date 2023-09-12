8f460b94dbaa065522d9e8aa5b5905464be16a3c
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-12 13:57:07 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+long int a = 1, b = 2, sum = 0;
+long int temp;
+
+while (a <= 4000000)
+{
+if (a % 2 == 0)
+{
+sum += a;
+}
+temp = a + b;
+a = b;
+b = temp;
+}
+
+printf("%ld\n", sum);
+
+return (0);
+}
