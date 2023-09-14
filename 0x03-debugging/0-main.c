ca32625677e7e17e12b10ec4384ee9b34b815a4a
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-14 14:21:04 -0700

    0-main.c, main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,20 @@
 #include "main.h"
-  
+
+
 /**
- * main-Test function for positive or negative
- * return: 0
+ * main - Entry point
+ * Description: This is the main function used for
+ * testing the positive_or_negative function.
+ * It initializes an integer variable `i` to 0 and
+ * then calls the `positive_or_negative` function.
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-        int 1;
-        i=0;
-        positive_or_negative(i);
+int 1;
+i = 0;
+positive_or_negative(i);
 
 return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
-void positive_or_negative(int 1);
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remainning_days(int month, int day, int year);
 int convert_day(int month, int year);
