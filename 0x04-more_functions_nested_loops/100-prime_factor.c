e151528337b21256144bd8cded28387629715358
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-14 16:57:02 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,41 +3,34 @@
 
 /**
  * main - Entry point
- * largest_prime_factor - Finds the largest prime factor of a number
- * @n: The number to find the largest prime factor of
- *
- * Return: The largest prime factor of the number n
+ * Return: Always 0 (Success)
  */
-long largest_prime_factor(long n)
-{
-long max_prime = -1;
-while (n % 2 == 0)
-{
-max_prime = 2;
-n /= 2;
-}
-
-for (long i = 3; i <= sqrt(n); i += 2)
-{
-while (n % i == 0)
-{
-max_prime = i;
-n = n / i;
-}
-}
-
-if (n > 2)
-{
-max_prime = n;
-}
-
-return (max_prime);
-}
-
 int main(void)
 {
-long number = 612852475143;
-long result = largest_prime_factor(number);
-printf("%ld\n", result);
+long num = 612852475143;
+long largest_prime;
+long i;
+
+/* divide num by 2 until it is odd */
+while (num % 2 == 0)
+{
+largest_prime = 2;
+num /= 2;
+}
+
+for (i = 3; i <= sqrt(num); i += 2)
+{
+while (num % i == 0)
+{
+largest_prime = i;
+num /= i;
+}
+}
+
+if (num > 2)
+largest_prime = num;
+
+printf("%ld\n", largest_prime);
+
 return (0);
 }
