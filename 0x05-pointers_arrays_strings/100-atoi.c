a46991e8c4fb7a9ac945cdbea3726b373f727e32
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-18 12:13:11 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer.
@@ -8,7 +9,6 @@
  */
 int _atoi(char *s)
 {
-
 /* Initialize the sign to positive*/
 int sign = 1;
 int result = 0;
@@ -23,6 +23,16 @@ else if (*s >= '0' && *s <= '9')
 
 /* Convert character to integer*/
 digit = *s - '0';
+
+/* Check for potential overflow*/
+if ((sign == 1 && (result > INT_MAX / 10)) ||
+(sign == 1 && (result == INT_MAX / 10 && digit > 7)) ||
+(sign == -1 && (result < INT_MIN / 10)) ||
+(sign == -1 && (result == INT_MIN / 10 && digit > 8)))
+{
+return ((sign == 1) ? INT_MAX : INT_MIN);
+}
+
 result = result * 10 + digit;
 }
 
