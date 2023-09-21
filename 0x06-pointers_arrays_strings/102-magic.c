c61e0bd82fcbc8b3b8a8bbb79b01cef49a9fb7f6
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-21 13:49:28 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,18 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int n;
 int a[5];
 int *p;
 
 a[2] = 1024;
-p = &n;
-*(p + 5) = 98;
+p = &a[2];
+*((int *)((char *)p + sizeof(int))) = 98; /* Add this line */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
