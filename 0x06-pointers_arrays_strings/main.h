56b5af1d5394ace2d7cbea599b7d4d8b9bd8d5c8
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-21 13:43:42 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,6 +16,7 @@ char *leet(char *a);
 char *rot13(char *a);
 void *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
+void print_buffer(char *b, int size);
 
 
 #endif /* MAIN_H */
