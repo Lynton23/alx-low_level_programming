1a2811987cfea2a74734023f454e8aaf987c4285
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-29 14:42:41 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change for an amount of money.
+ * main - prints the minimum number of
+ *coins to make change for an amount of money.
  * @argc: argument count
  * @argv: argument vector
  * Return: 0 if successful, 1 otherwise
