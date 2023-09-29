764bedadf76ab27e3e4d05f9fa1b2978e2b15de6
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-29 14:03:56 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,43 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry point of the program
- * @argc: The number of command-line arguments (unused)
- * @argv: An array of strings containing the command-line arguments
- *
- * Return: 0 on success
+ * main - prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful
  */
 int main(int argc, char *argv[])
 {
-(void)argc;
-print_program_name(argv);
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+
 return (0);
 }
 
-/**
- * print_program_name - Prints the program name followed by a new line
- * @argv: An array of strings containing the command-line arguments
- */
-void print_program_name(char *argv[])
-{
-int i = 0;
-
-while (argv[0][i] != '\0')
-{
-_putchar(argv[0][i]);
-i++;
-}
-_putchar('\n');
-}
-
-/**
- * _putchar - Writes a character to the standard output (stdout)
- * @c: The character to write
- *
- * Return: 1 on success, -1 on error
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
