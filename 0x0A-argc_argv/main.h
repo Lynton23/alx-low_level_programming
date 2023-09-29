2ff9c59a1085c9069cb9ec79865df71d936ef886
Lynton23 <kiplangatlynton23@gmail.com>
2023-09-29 14:11:48 -0700

    2-args.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 int _putchar(char c);
 void print_program_name(char *argv[]);
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
 
 #endif /* MAIN_H */
