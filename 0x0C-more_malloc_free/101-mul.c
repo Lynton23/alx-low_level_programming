df50f96af3aeeacadd764a93275895a3082a24c9
Lynton23 <kiplangatlynton23@gmail.com>
2023-10-05 12:05:13 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,44 +2,38 @@
 #include <stdlib.h>
 
 /**
- * is_digit - Checks if a string consists of only digits.
- * @str: The string to check.
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: 1 if the string consists of only digits, 0 otherwise.
- */
-int is_digit(char *str)
-{
-while (*str)
-{
-if (*str < '0' || *str > '9')
-return (0);
-str++;
-}
-return (1);
-}
-
-/**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- *
- * Return: 0 on success, 98 on error.
+ * Return: 0 on success, 98 on error
  */
 int main(int argc, char *argv[])
 {
-unsigned long num1, num2, result;
+int i, j;
+long num1, num2;  /* Declare variables here */
 
-if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+if (argc != 3)
 {
 printf("Error\n");
 return (98);
 }
 
-num1 = strtoul(argv[1], NULL, 10);
-num2 = strtoul(argv[2], NULL, 10);
-result = num1 * num2;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return (98);
+}
+}
+}
 
-printf("%lu\n", result);
+num1 = atol(argv[1]);  /* Assign values here */
+num2 = atol(argv[2]);
+printf("%ld\n", num1 *num2);
 
 return (0);
 }
