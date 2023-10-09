a84fbdc773b2f1027fa460f02400d4096e6fe6b0
Lynton23 <kiplangatlynton23@gmail.com>
2023-10-09 11:22:30 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,6 @@ char *owner;
 /* Function prototype */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 #endif /* DOG_H */
 
